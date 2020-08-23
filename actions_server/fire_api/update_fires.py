98aa1a00a8e08cbf5e383f2485a94f3a96b4f6f3
Amit Tallapragada <amittallapragada@gmail.com>
2020-08-23 15:30:55 -0700

    update cron jobs

--- actions_server/fire_api/update_fires.py
+++ actions_server/fire_api/update_fires.py
@@ -22,29 +22,33 @@ r = requests.get(url, allow_redirects=True)
 
 fire_data = json.loads(r.content)
 
+print("Update fires: Starting run at", datetime.now())
+
 for fire in fire_data['ListIncidents']:
-	try:
-		for county in fire['Counties']:
-			item = {}
-			item['county'] = county
-			item['uuid'] = fire['UniqueId']
-			item['url'] = 'https://www.fire.ca.gov/' + fire['CanonicalUrl']
-			item['name'] = fire['Name']
-			item['description'] = fire['SearchDescription']
-			item['status'] = fire['Status']
-			item['final'] = fire['Final']
+        try:
+            for county in fire['Counties']:
+                item = {}
+                item['county'] = county
+                item['uuid'] = fire['UniqueId']
+                item['url'] = 'https://www.fire.ca.gov/' + fire['CanonicalUrl']
+                item['name'] = fire['Name']
+                item['description'] = fire['SearchDescription']
+                item['status'] = fire['Status']
+                item['final'] = fire['Final']
 
-			item['updated'] = fire['Updated']
+                item['updated'] = fire['Updated']
 
-			item['started'] = Decimal(datetime.strptime(fire['Started'][:19] + 'Z', '%Y-%m-%dT%H:%M:%SZ').timestamp())
-			item['updated'] = Decimal(datetime.strptime(fire['Updated'][:19] + 'Z', '%Y-%m-%dT%H:%M:%SZ').timestamp())
+                item['started'] = Decimal(datetime.strptime(fire['Started'][:19] + 'Z', '%Y-%m-%dT%H:%M:%SZ').timestamp())
+                item['updated'] = Decimal(datetime.strptime(fire['Updated'][:19] + 'Z', '%Y-%m-%dT%H:%M:%SZ').timestamp())
 
-			if fire['AcresBurned']:
-				item['acres-burned'] = Decimal(fire['AcresBurned'])
-			if fire['PercentContained']:
-				item['percent-contained'] = Decimal(fire['PercentContained'])
-			
-			item['record-updated'] = int(datetime.now().timestamp())
-			fires_table.put_item(Item=item)
-	except Exception:
-		pass
+                if fire['AcresBurned']:
+                        item['acres-burned'] = Decimal(fire['AcresBurned'])
+                if fire['PercentContained']:
+                        item['percent-contained'] = Decimal(fire['PercentContained'])
+                
+                item['record-updated'] = int(datetime.now().timestamp())
+                print(item)
+                fires_table.put_item(Item=item)
+        except Exception as e:
+                print("update fires Exception!", e)
+                pass
