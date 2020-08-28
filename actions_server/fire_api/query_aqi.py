6c970637b5972fcd7f09554e5116650b133e07c3
Amit Tallapragada <amittallapragada@gmail.com>
2020-08-27 20:20:56 -0700

    cleaning repo

--- actions_server/fire_api/query_aqi.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://airnowgovapi.com/reportingarea/get"
-payload = {'latitude': '37.87320','longitude': '122.27296', 'stateCode': 'CA', 'maxDistance': '50'}
-
-response = requests.post(url, data = payload)
-print(response.content)
